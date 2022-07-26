1fbfffddc6838d78c56a3b4b73c471b916695b67
nayimHossen <nayimh2015@gmail.com>
2022-07-27 05:56:26 +0600

    ready for start codeing

---
+++ car-rental-frontend/src/index.js
@@ -0,0 +1,18 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "remixicon/fonts/remixicon.css";
+import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
+import App from "./App";
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+);
